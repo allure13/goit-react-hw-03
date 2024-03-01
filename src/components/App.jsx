2ae969148cb2ba563237c20ec729446ff974bd3a
Ihor Chepliaka <ihor.chepliaka@gmail.com>
2024-03-01 20:41:18 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,59 @@
 import './App.css';
-import userData from '../userData.json';
-import Profile from './Profile/Profile';
-import friends from '../friends.json';
-import FriendList from './FriendList/FriendList';
-import TransactionHistory from './TransactionHistory/TransactionHistory';
-import transactions from '../transactions.json';
+import ContactList from './ContactList/ContactList';
+import initialContacts from '../contactlist.json';
+import SearchBar from './SearchBox/SearchBox';
+import { useEffect, useState } from 'react';
+import ContactForm from './ContactForm/ContactForm';
 
-const App = () => {
-  return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
+export default function App() {
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) {
+      return JSON.parse(savedContacts);
+    } else {
+      return initialContacts;
+    }
+  });
 
-      <FriendList friends={friends} />
+  const [searchTerm, setSearchTerm] = useState('');
 
-      <TransactionHistory items={transactions} />
-    </>
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
-};
 
-export default App;
+  const addContact = newContact => {
+    const isDuplicateNumber = contacts.some(
+      contact => contact.number === newContact.number
+    );
+    if (isDuplicateNumber) {
+      alert('This phone number already exists!');
+    } else {
+      setContacts(prevContacts => {
+        return [...prevContacts, newContact];
+      });
+    }
+  };
+
+  const deleteContact = contactId => {
+    setContacts(prevContacts => {
+      return prevContacts.filter(contact => contact.id !== contactId);
+    });
+  };
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onAdd={addContact} />
+      <SearchBar searchTerm={searchTerm} onChange={handleChange} />
+      <ContactList contacts={visibleContacts} onDeleteContact={deleteContact} />
+    </div>
+  );
+}
